7e22bbd4bfb1d5556d314575409e854423f6a467
herbanarchitech <jeff.badette@protonmail.com>
2021-12-10 17:42:17 -0500

    very first commit. Simple tunings. Adding the controller

---
+++ MediaServer/MediaFileServer/src/main/java/com/example/MediaServer/MediaFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaFileServerApplication.class, args);
+	}
+
+}
---
+++ MediaServer/MediaFileServer/src/test/java/com/example/MediaServer/MediaFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
